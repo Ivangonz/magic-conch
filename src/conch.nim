8b0e8c8eedeaa32e2d3bdfa4cf6fad6babf292a4
Ivan Gonzalez <Ivangonz@outlook.com>
2022-05-09 22:55:35 -0600

    Removed http client and imported data as json

--- src/conch.nim
+++ src/conch.nim
@@ -1,27 +1,32 @@
-import dimscord, asyncdispatch, options, dimscmd, std/random, std/os, dotenv
+import dimscord, asyncdispatch, options, dimscmd, std/random, std/os, dotenv, json
 
 load()
 randomize()
 let TOKEN = os.getEnv("DISCORD_TOKEN")
+
 let discord = newDiscordClient(TOKEN)
 var cmd = discord.newHandler()
-let responses = [
-  "Yes",
-  "No",
-  "What are you stupid?",
-  "I don't know man",
-  "Yer",
-  "I guess",
-]
+
+let responses = parseFile("src/data/responses.json")
+let fortunes = parseFile("src/data/fortunes.json")
+let tarotCards = parseFile("src/data/tarot.json")
+
 var response: string
+var fortune: string
 proc onReady(s: Shard, r: Ready) {.event(discord).} =
   echo "Ready as " & $r.user
 
 proc messageCreate (s: Shard, msg: Message) {.event(discord).} =
-  response = responses[rand(responses.len - 1)]
   discard await cmd.handleMessage(".", s, msg)
 
-cmd.addChat("concha") do ():
+cmd.addChat("concha fortune") do ():
+  fortune = fortunes[rand(fortunes.len - 1 )].getStr()
+  discard await discord.api.sendMessage(msg.channelId, fortune)
+
+cmd.addChat("concha question") do ():
+  response = responses[rand(responses.len - 1)].getStr()
   discard await discord.api.sendMessage(msg.channelId, response)
 
 waitFor discord.startSession()
+# echo responses
+# echo responses[rand(responses.len - 1)].getStr()
